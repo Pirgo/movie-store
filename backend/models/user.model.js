b279865e7c8b86d9754b40b10753a895e63ea66b
xsanm <kkurowski.kk@gmail.com>
2021-05-18 15:19:37 +0200

    adding user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -3,13 +3,75 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: {
+  email: {
     type: String,
-    required: true,
-    unique: true,
     trim: true,
-    minlength: 3
+    unique: true,
+    required: [true, 'email is required'],
+    match: [/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/, 'Please fill a valid email address']
   },
+  password: {
+    type: String,
+    required: true
+  },
+  firstName: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+
+  lastName: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  library: {
+    toWatch: [{
+      _id: false,
+      movieID: {
+        type: String,
+        required: true
+      },
+      title: {
+        type: String,
+        required: true,
+        trim: true
+      }
+    }],
+    favourites: [{
+      _id: false,
+      movieID: {
+        type: String,
+        required: true
+      },
+      title: {
+        type: String,
+        required: true,
+        trim: true
+      }
+    }],
+    seen: [{
+      _id: false,
+      movieID: {
+        type: String,
+        required: true
+      },
+      title: {
+        type: String,
+        required: true,
+        trim: true
+      },
+      rate: {
+        type: Number,
+        min: 0,
+        max: 5
+      }
+    }]
+
+  }
+
+
+
 }, {
   timestamps: true,
 });
