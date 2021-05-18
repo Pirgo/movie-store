812aaa14febc19916369143f30d9e17b5395068e
Pirgo <pierogmateusz@wp.pl>
2021-05-18 13:51:39 +0200

    movie model done

--- backend/models/movie.model.js
+++ backend/models/movie.model.js
@@ -8,48 +8,91 @@ const movieSchema = new Schema({
     },
     genre: {
         type: [String],
-        required: [true, 'genre field is required'],
         validate: {
             validator: function (v) {
+                v = v.map((g) => g.toLowerCase());
                 return (new Set(v)).size === v.length;
             },
             message: props => `${props.value} duplicated movie genres!`
-        }
-      }
-    
-    // "date": "",
-    //     "runtime": "",
-    //     "plot": "",
-    //     "rate": {
-    //         "sum": "",
-    //         "amount": ""
-    //     },
-    //     "directors": [
-    //         {
-    //             "directorID": ""
-    //         }
-    //     ],
-    //     "writers": [
-    //         {
-    //             "writerID": ""
-    //         }
-    //     ],
-    //     "actors": [
-    //         {
-    //             "actorID": ""
-    //         }
-    //     ],
-    //     "cover": "",
-    //     "platforms": [
+        },
+        required: true
+    },
 
-    //     ],
-    //     "platforms": [
-    //         {
-    //             "name": "",
-    //             "URL": ""
-    //         }
-    //     ]
-    }, {
+    date: {
+        type: Date,
+        required: true
+
+    },
+    runtime: {
+        type: Number,
+        required: true,
+        min: 1
+    },
+    plot: {
+        type: String,
+        default: "To be added"
+    },
+    rate: {
+        sum: {
+            type: Number,
+            required: true,
+            min: 0
+        },
+        amount: {
+            type: Number,
+            required: true,
+            min: 0
+        }
+    },
+    directors: [{
+        _id: false,
+        directorID: {
+            type: String
+        },
+        directorName: {
+            type: String,
+            default: "Anonymous director"
+        }
+    }],
+    writers: [{
+        _id: false,
+        writerID: {
+
+            type: String
+        },
+        writerName: {
+            type: String,
+            default: "Anonymous writer"
+        }
+    }],
+    actors: [{
+        _id: false,
+        actorID: {
+            type: String
+        },
+        actorName: {
+            type: String,
+            default: "Anonymous actor"
+        }
+    }],
+
+    cover: {
+        type: String,
+        default: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTFlOaeron7sa8fd9Xlv5GZZ3JOkd6piSEMpw&usqp=CAU"
+    },
+
+    platforms: [{
+        _id: false,
+        name: {
+            type: String,
+            required: true
+        },
+        URL: {
+            type: String,
+            required: true
+        }
+    }]
+}, {
     timestamps: true,
 });
 
--- backend/routes/movie.js
+++ backend/routes/movie.js
@@ -2,20 +2,44 @@ const router = require('express').Router();
 let Movie = require('../models/movie.model');
 
 router.route('/').get((req, res) => {
-  Movie.find()
-    .then(users => res.json(users))
-    .catch(err => res.status(400).json('Error: ' + err));
+    Movie.find()
+        .then(users => res.json(users))
+        .catch(err => res.status(400).json('Error: ' + err));
 });
 
 router.route('/add').post((req, res) => {
-  const title = req.body.title;
-  const genre = req.body.genre;
+    const title = req.body.title;
+    const genre = req.body.genre;
+    const date = req.body.date;
+    const runtime = req.body.runtime;
+    const plot = req.body.plot
+    const rate = req.body.rate;
+    const directors = req.body.directors;
+    const writers = req.body.writers;
+    const actors = req.body.actors;
+    const cover = req.body.cover;
+    const platforms = req.body.cover;
 
-  const newMovie = new Movie({title, genre});
+    // const newMovie = new Movie({
+    //     title,
+    //     genre,
+    //     date, 
+    //     runtime, 
+    //     plot, 
+    //     rate,
+    //     directors,
+    //     writers,
+    //     actors,
+    //     cover,
+    //     platforms
+    // });
+    const newMovie = new Movie({
+            ...req.body
+        });
 
-  newMovie.save()
-    .then(() => res.json('Movie added!'))
-    .catch(err => res.status(400).json('Error: ' + err));
+    newMovie.save()
+        .then(() => res.json('Movie added!'))
+        .catch(err => res.status(400).json('Error: ' + err));
 });
 
 module.exports = router;
